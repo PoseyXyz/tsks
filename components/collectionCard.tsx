2023377370e57ad602dac81fb249fbe1f5188030
Adeposi <adeposi60@gmail.com>
2022-12-22 16:15:38 +0400

    Completed styling of collection card

---
+++ components/collectionCard.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BsPlusLg } from "react-icons/bs";
+
+function CollectionCard({name, iconBg, completed_tasks, total_tasks, border, hover}) { 
+ 
+    return (
+        <div className={`bg-[#20212C] flex flex-col gap-4 p-4 rounded-2xl duration-700 cursor-pointer hover:-translate-y-3 ${hover} shadow-2xl border border-transparent`}>
+            <button className={`shadow-2xl ${iconBg} p-4 rounded-2xl inline self-start mb-8`}>
+                <i><BsPlusLg /></i>
+            </button>
+
+            <span className="text-xl font-black">{name}</span>
+            <div className="flex justify-between items-center">
+                <span className="text-[#9b9aa1]">{completed_tasks}/{total_tasks} done</span>
+                <div className="w-4 h-4 p-4 border-white border-2 rounded-full flex items-center justify-center">
+                    <div className={`w-2/4 h-2/4 ${border} p-4 border-2 rounded-full`}>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CollectionCard
--- pages/collections.tsx
+++ pages/collections.tsx
@@ -1,12 +1,31 @@
 import React from "react";
-import {BsFillGrid1X2Fill} from 'react-icons/bs'
+import { FaEllipsisH } from 'react-icons/fa'
 import DashNav from "../components/dashNav";
+import CollectionCard from "../components/collectionCard";
+import { BsPlusLg } from 'react-icons/bs'
 
-function Collections(){
+function Collections() {
     return (
         <main>
-            <section>
-                <DashNav/>
+            <DashNav />
+            <section className="container flex flex-col gap-8 max-w-4xl px-6 md:px-8 py-12 mb-24 md:mb-0">   
+                <div className="flex justify-between items-center mb-10">
+                    <h2 className="text-4xl text-center">Collections</h2>
+                    <button className="hover:text-white text-[#585A60] duration-500"><i><FaEllipsisH /></i></button>
+                </div>
+
+                <div className="flex gap-4">
+                    <button className="px-4 py-2 border-2 border-[#403F51] text-white hover:bg-[#403F51] duration-500 rounded-2xl">Favourites</button>
+                    <button className="px-4 py-2 bg-[#403F51] text-white duration-500 hover:bg-transparent hover:border-[#403F51] border-2 border-transparent rounded-2xl">All collections</button>
+                </div>
+                
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                    <CollectionCard name='School' border='border-[#F8769F]' hover='hover:border-[#F8769F]' completed_tasks={4} total_tasks={8} iconBg='bg-[#F8769F]'/>
+                    <CollectionCard name='Personal' border='border-[#76C0BB]' hover='hover:border-[#76C0BB]' completed_tasks={3} total_tasks={5} iconBg='bg-[#76C0BB]'/>
+                    <CollectionCard name='Design' border='border-[#AA6BDB]' hover='hover:border-[#AA6BDB]' completed_tasks={15} total_tasks={15} iconBg='bg-[#AA6BDB]'/>
+                    <CollectionCard name='Groceries' border='border-[#CDB350]' hover='hover:border-[#CDB350]' completed_tasks={2} total_tasks={10} iconBg='bg-[#CDB350]'/>
+                    <button className="border-2 border-[#403F51] text-[#403F51] h-1/2 hover:border-white hover:h-full hover:text-white p-8 flex justify-center items-center rounded-2xl duration-500 flex-col gap-4 group"><i className="group-hover:animate-spin"><BsPlusLg/></i><span className="group-hover:block hidden">Add new collection</span></button>
+                </div>
             </section>
         </main>
     )
